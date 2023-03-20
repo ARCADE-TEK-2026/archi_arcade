03ad38cf1a999fd8ec15c2fba53a6da1d99962d8
Xavier Mitault <xavier.mitault@epitech.eu>
2023-03-20 21:35:38 +0100

    Make format

--- include/IDisplayModule.hpp
+++ include/IDisplayModule.hpp
@@ -21,19 +21,24 @@ namespace Arcade {
                 virtual ~IDisplayModule() = default;
 
                 /**
-                 * @brief Get the system manager of the current graphical library
+                 * @brief Get the system manager of the current graphical
+                 * library
                  *
                  * @return The system manager of the current graphical library
                  */
                 virtual ECS::ISystemManager &getSystemManager() = 0;
                 /**
-                 * @brief Change the current active graphical library to the given graphical library
+                 * @brief Change the current active graphical library to the
+                 * given graphical library
                  *
-                 * @param libGraphicPath The path of the graphical library to change to (.so)
+                 * @param libGraphicPath The path of the graphical library to
+                 * change to (.so)
                  *
-                 * @return True if the graphical library was changed, false otherwise
+                 * @return True if the graphical library was changed, false
+                 * otherwise
                  */
-                virtual bool changeGaphicLib(const std::string &libGraphicPath) = 0;
+                virtual bool changeGaphicLib(
+                const std::string &libGraphicPath) = 0;
         };
-    }
-}
+    } // namespace Core
+} // namespace Arcade
