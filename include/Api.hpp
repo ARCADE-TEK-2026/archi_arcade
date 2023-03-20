55b9060e0787aeb7d13136c5f30cbde3c672be0f
guillaume@isee-u.fr <guillaume@isee-u.fr>
2023-03-20 21:00:58 +0100

    Add Api.hpp

---
+++ include/Api.hpp
@@ -0,0 +1,57 @@
+/*
+** EPITECH PROJECT, 2023
+** Visual Studio Live Share (Workspace)
+** File description:
+** ICore
+*/
+
+#pragma once
+
+#include "ISystemManager.hpp"
+
+enum class libType {
+    GRAPH,
+    GAME
+};
+
+extern "C" {
+    /** 
+     * @brief game shared lib main entry point to get scenes
+     * 
+     * @return a unique_ptr on ISceneManager instance
+     * 
+     * All game shared lib must have this function
+     *
+    */
+    //std::unique_ptr<ISceneManager> &
+
+    /** 
+     * @brief graph shared lib main entry point to get systems
+     * 
+     * @return a unique_ptr on ISystemManager instance
+     * 
+     * All graph shared lib must have this function
+     *
+    */
+    std::unique_ptr<Arcade::ECS::ISystemManager> getSystems();
+
+    /**
+     * @brief shared lib entry point to get lib name
+     * 
+     * @return lib's name in std::string 
+     * 
+     * All shared lib must have this function
+     *
+    */
+    std::string getName();
+
+    /**
+     * @brief shared lib entry point to get lib type
+     * 
+     * @return libType enum
+     * 
+     * All shared lib must have this function
+     *
+    */
+    libType getType();
+}
