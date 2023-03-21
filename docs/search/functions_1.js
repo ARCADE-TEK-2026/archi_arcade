c66b8555bc7200188a0c8039850c1d3112f08381
Github Action documentation <>
2023-03-21 10:17:01 +0000

    Deploying to gh-pages from @ ARCADE-TEK-2026/archi_arcade@f27405b4f1e9ea2e854a9bb3dcda49d970964b49 🚀

--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['changegame_179',['changeGame',['../a00899.html#a6edd79d041e5bac4b35a3476017d00f2',1,'Arcade::Core::IGameModule']]],
-  ['changegaphiclib_180',['changeGaphicLib',['../a00895.html#aeec7f6b8d343e6825ee75c5c3c3e2225',1,'Arcade::Core::IDisplayModule::changeGaphicLib(const std::string &amp;libGraphicName)=0'],['../a00895.html#ac404cd7f958e1601997128d688c41881',1,'Arcade::Core::IDisplayModule::changeGaphicLib()=0']]],
+  ['changegraphiclib_180',['changeGraphicLib',['../a00895.html#a2d6093dd2989a9441aa671d5b01c4663',1,'Arcade::Core::IDisplayModule::changeGraphicLib(const std::string &amp;libGraphicName)=0'],['../a00895.html#a087e08ef15a6d0d0c018c5a5c57e040a',1,'Arcade::Core::IDisplayModule::changeGraphicLib()=0']]],
   ['changescene_181',['changeScene',['../a00931.html#a847f3b7edc12ba7d66f303ca7996a0b7',1,'Arcade::Game::ISceneManager']]],
   ['clearevents_182',['clearEvents',['../a00915.html#a31d98f0497d5712053e1bf53f01325e0',1,'Arcade::ECS::IEventManager']]],
   ['close_183',['close',['../a00919.html#ace6ce33909ed5250419f94c931a0cd6a',1,'Arcade::ECS::ISystem::close()'],['../a00927.html#a69a2bdefaef1a022a895ae7e713a6227',1,'Arcade::Game::IScene::close()']]],
-  ['createentity_184',['createEntity',['../a00911.html#a863a130b3211829514e28cb11c8b397e',1,'Arcade::ECS::IEntityManager']]]
+  ['createentity_184',['createEntity',['../a00911.html#a018b7eb67da2e67515ad8fe957fd8c30',1,'Arcade::ECS::IEntityManager']]]
 ];
