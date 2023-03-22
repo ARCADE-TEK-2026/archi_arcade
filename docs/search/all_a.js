726d732f2f0070ace6ada7fa6dc8e06b622ba8f4
Github Action documentation <>
2023-03-22 19:05:41 +0000

    Deploying to gh-pages from @ ARCADE-TEK-2026/archi_arcade@84514445941c916744b7d28361860da66c66ce8b 🚀

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -4,7 +4,7 @@ var searchData=
   ['rect_91',['Rect',['../a00943.html',1,'Arcade::Graph']]],
   ['registerscene_92',['registerScene',['../a00931.html#a896913596057a8c4e5dbdd72093a730b',1,'Arcade::Game::ISceneManager']]],
   ['removeallentities_93',['removeAllEntities',['../a00911.html#a6542722832aa9bd45b62b92d5c35201e',1,'Arcade::ECS::IEntityManager']]],
-  ['removecomponent_94',['removeComponent',['../a00907.html#ae1b0864f76fa9ad515adb5514bc8e43f',1,'Arcade::ECS::IEntity']]],
+  ['removecomponent_94',['removeComponent',['../a00907.html#a6cf0c632639abbfb76a266d007bed227',1,'Arcade::ECS::IEntity']]],
   ['removecomponents_95',['removeComponents',['../a00907.html#abddaeef7a17806c4502c61952e3b4ee2',1,'Arcade::ECS::IEntity']]],
   ['removeentity_96',['removeEntity',['../a00911.html#a4ecaa745c626995efdab0859d9d02e4d',1,'Arcade::ECS::IEntityManager']]],
   ['removesystem_97',['removeSystem',['../a00923.html#a1b1050fb958386f47e312c8c62bf2802',1,'Arcade::ECS::ISystemManager']]],
--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['registerscene_221',['registerScene',['../a00931.html#a896913596057a8c4e5dbdd72093a730b',1,'Arcade::Game::ISceneManager']]],
   ['removeallentities_222',['removeAllEntities',['../a00911.html#a6542722832aa9bd45b62b92d5c35201e',1,'Arcade::ECS::IEntityManager']]],
-  ['removecomponent_223',['removeComponent',['../a00907.html#ae1b0864f76fa9ad515adb5514bc8e43f',1,'Arcade::ECS::IEntity']]],
+  ['removecomponent_223',['removeComponent',['../a00907.html#a6cf0c632639abbfb76a266d007bed227',1,'Arcade::ECS::IEntity']]],
   ['removecomponents_224',['removeComponents',['../a00907.html#abddaeef7a17806c4502c61952e3b4ee2',1,'Arcade::ECS::IEntity']]],
   ['removeentity_225',['removeEntity',['../a00911.html#a4ecaa745c626995efdab0859d9d02e4d',1,'Arcade::ECS::IEntityManager']]],
   ['removesystem_226',['removeSystem',['../a00923.html#a1b1050fb958386f47e312c8c62bf2802',1,'Arcade::ECS::ISystemManager']]],
