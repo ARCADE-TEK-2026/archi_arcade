49acbc100fcf9abc6adfa28c992af18a582d88b3
Xavier Mitault <xavier.mitault@epitech.eu>
2023-03-23 11:25:30 +0100

    Fix issue about doc

--- include/Api.hpp
+++ include/Api.hpp
@@ -32,6 +32,9 @@ extern "C"
      *
      * All game shared lib must have this function
      *
+     * It takes a parameter because, if there was no parameter, we will have to implement all the Archi commune in all .so (to be hable to create (malloc/new) one of them
+     * Now, with this parameter, we don't have to code everything in .so
+     *
      */
     EXPORT std::unique_ptr<Arcade::Game::ISceneManager> getScenes(
     std::unique_ptr<Arcade::Game::ISceneManager> sceneManager);
@@ -42,6 +45,9 @@ extern "C"
      * @return A unique_ptr on ISystemManager instance
      *
      * All graph shared lib must have this function
+     *
+     * It takes a parameter because, if there was no parameter, we will have to implement all the Archi commune in all .so (to be hable to create (malloc/new) one of them
+     * Now, with this parameter, we don't have to code everything in .so
      *
      */
     EXPORT std::unique_ptr<Arcade::ECS::ISystemManager> getSystems(
